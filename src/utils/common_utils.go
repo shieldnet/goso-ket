8c1d9aeec16e4158924415aef1279ef1f0be5e4c
Seongmin Kim <atez.dev@gmail.com>
2021-01-18 23:32:57 +0900

    Seperate common utils.go

---
+++ src/utils/common_utils.go
@@ -0,0 +1,11 @@
+package main
+
+import "net"
+
+func GetListenerIpAddress(ip, port string) string {
+	return ip + ":" + port
+}
+
+func MakeTcpListener(address string) (net.Listener, error){
+	return net.Listen("tcp", address)
+}
