dfd8ff68eca66b94fea39978aa1726429df85c09
Seongmin Kim <atez.dev@gmail.com>
2021-06-08 04:05:35 +0900

    Add command handler

--- hw4/TCPServer/chat_server.go
+++ hw4/TCPServer/chat_server.go
@@ -15,19 +15,19 @@ type ChatServer struct {
 	Port               string
 	Listener           net.Listener
 	Clients            map[string]*Client
-	Commands           map[string]func(Request) Response
-	HiddenCommands 	   map[string]func(Request) Response
+	Commands           map[string]func(Request, *Client) Response
+	HiddenCommands     map[string]func(Request, *Client) Response
 	TotalClientsNumber int32
 	NthClient          int32
 }
 
 type Handler interface {
-	GetUserList(req Request) Response
-	Whisper(req Request) Response
-	Exit(req Request) Response
-	GetVersion(req Request) Response
-	ChangeName(req Request) Response
-	GetRtt(req Request) Response
+	GetUserList(req Request, client *Client) Response
+	Whisper(req Request, client *Client) Response
+	Exit(req Request, client *Client) Response
+	GetVersion(req Request, client *Client) Response
+	ChangeName(req Request, client *Client) Response
+	GetRtt(req Request, client *Client) Response
 }
 
 var basicChatServer *ChatServer = nil
@@ -55,12 +55,18 @@ func InitChatServer(ip, port string) *ChatServer {
 	basicChatServer = cs
 
 	// Commmand
-	cs.Commands = map[string]func(request Request) Response{
+	cs.Commands = map[string]func(request Request, client *Client) Response{
 		"\\version": cs.GetVersion,
 		"\\users":   cs.GetUserList,
 		"\\wh":      cs.Whisper,
 		"\\rename":  cs.ChangeName,
 		"\\rtt":     cs.GetRtt,
+	}
+
+	// Hidden Command
+	cs.HiddenCommands = map[string]func(request Request, client *Client) Response{
+		"\\join": cs.Join,
+		"\\say":  cs.Say,
 	}
 	return cs
 }
@@ -107,20 +113,96 @@ func (c *ChatServer) Handle(conn net.Conn, client *Client) {
 
 		if count > 0 {
 			data := buffer[:count]
-			msg := Request{}
+			req := Request{}
 			resp := Response{}
 
-			if err := json.Unmarshal(data, &msg); err != nil {
+			if err := json.Unmarshal(data, &req); err != nil {
 				log.Fatalln(err)
 				return
 			}
 
 			// Handle Commands
+			for k := range c.Commands {
+				if req.Command == k {
+					resp = c.Commands[k](req, client)
+				}
+			}
 
+			// Handle Hidden Commands
+			for k := range c.HiddenCommands {
+				if req.Command == k {
+					resp = c.HiddenCommands[k](req, client)
+				}
+			}
+
+			if resp.Status == "" {
+				resp.Status = "404"
+				resp.Message = fmt.Sprintf(`The Command "%s" is not exist.`, req.Command)
+			}
 
 			b, _ := json.Marshal(resp)
 			conn.Write(b)
 		}
 
 	}
+}
+
+func (c *ChatServer) Join(req Request, client *Client) Response {
+	res := Response{}
+
+	if req.Command == "\\join" {
+		c.Clients[req.Param["name"]] = client
+	} else {
+		res.Status = "400"
+		res.Message = ""
+	}
+	return res
+}
+
+func (c *ChatServer) Say(req Request, client *Client) Response {
+	res := Response{}
+
+	clientRequest := Request{
+		Command: "\\say",
+		Param: map[string]string{
+			"from":    client.Name,
+			"message": req.Param["message"],
+		},
+	}
+
+	b, _ := json.Marshal(clientRequest)
+
+	for _, cl := range c.Clients {
+		cl.Connection.Write(b)
+	}
+
+	res.Message = "Completed"
+	res.Status = "200"
+
+	return res
+}
+
+func (c *ChatServer) GetVersion(req Request, client *Client) Response {
+	return Response{}
+}
+
+func (c *ChatServer) GetUserList(req Request, client *Client) Response {
+	return Response{}
+}
+
+func (c *ChatServer) Whisper(req Request, client *Client) Response {
+	return Response{}
+}
+
+func (c *ChatServer) ChangeName(req Request, client *Client) Response {
+	return Response{}
+}
+
+func (c *ChatServer) GetRtt(req Request, client *Client) Response {
+	return Response{}
+}
+
+func (c *ChatServer) Exit(req Request, client *Client) Response {
+
+	return Response{}
 }
